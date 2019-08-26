fa3c4bca0541f90e185861ee965804e112659ff3
MARCELO FRANCISCONI LUBANCO THOMÉ <lubanco@edu.unifil.br>
2019-08-25 22:03:19 -0300

    Implementa sistema operacional dinamico para linux e windows

--- SistemaOperacional.py
+++ SistemaOperacional.py
@@ -1,13 +1,9 @@
 import os
 
 print("Sistema Operacional")
-while True:
-    print("1- ls")
-    print("2- mkdir")
-    print("3- cd path")
-    print("4- pwd")
-    print("9- sair")
-    opcao = input("Opção: ")
+
+
+def sistemaOperacionalLinux(opcao):
     if opcao == "1":
         os.system("ls")
     elif opcao == "2":
@@ -19,7 +15,39 @@ while True:
     elif opcao == "4":
         os.system("pwd")
     elif opcao == "9":
-        print("Exit Sistema Operacional")
-        break
+        print("Exit Linux")
+
+def sistemaOperacionalWindows(opcao):
+    if opcao == "1":
+        os.system("dir")
+    elif opcao == "2":
+        diretorio = input("Nome do diretório: ")
+        os.mkdir(diretorio)
+    elif opcao == "3":
+        cd = input("Informe o path: ")
+        os.chdir(cd)
+    elif opcao == "4":
+        os.system("pwd")
+    elif opcao == "9":
+        print("Exit Windows")
+
+sistema = input("Windows ou Linux: ")
+while True:
+    print("1- ls")
+    print("2- mkdir")
+    print("3- cd path")
+    print("4- pwd")
+    print("9- sair")
+    opcao = input("Opção: ")
+    if sistema == "linux":
+        sistemaOperacionalLinux(opcao)
+    elif sistema == "windows":
+        sistemaOperacionalWindows(opcao)
+
+
+
+
+
+
 
 
